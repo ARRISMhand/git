a395183c6d659accca4d03144a4834e341ecfd98
m.arris <arris.mhand@gmail.com>
2021-05-24 22:25:54 +0200

    test commint github

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -2,7 +2,6 @@
 public class Test {
 	public static void main(String[] args) {
 
-        System.out.println("Salut momo, รงa va gros !? ce ci est mon deuxieme commit DEUXIEMDEOD IEJDI"); 
-		System.out.println("Modif brancehe Dev"); 
+		System.out.println("test githuuuub"); 
     }
 }
