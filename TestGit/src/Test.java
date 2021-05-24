fe9ea98058e86a3abd428566b403965e05c5b61e
m.arris <arris.mhand@gmail.com>
2021-05-24 21:23:04 +0200

    nouveau projet

---
+++ TestGit/src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	public static void main(String[] args) {
+
+        System.out.println("Salut momo, รงa va gros !? ce ci est mon deuxieme commit DEUXIEMDEOD IEJDI"); 
+		System.out.println("Modif brancehe Dev"); 
+    }
+}
--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-
-    
-    public static void main(String[] args) {
-
-        System.out.println("Salut momo, ça va gros !? ce ci est mon deuxieme commit DEUXIEMDEOD IEJDI"); 
-    }
-}
